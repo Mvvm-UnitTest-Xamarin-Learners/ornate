c5641e05383f93bf969340d498d6bf101e611048
Nachiappan <nachiapan@gmail.com>
2019-01-26 20:01:45 +0530

    Added support for measuring code coverage

--- DeviceControlApp.Core/ViewModel/RelayCommand.cs
+++ DeviceControlApp.Core/ViewModel/RelayCommand.cs
@@ -10,17 +10,12 @@ namespace DeviceControlApp.Core.ViewModel
 
         public RelayCommand(Action<object> execute, Predicate<object> canExecute)
         {
-            if (execute == null)
-                throw new ArgumentNullException("execute");
-
             _execute = execute;
             _canExecute = canExecute;
-
         }
 
         public RelayCommand(Action execute) : this((obj) => { execute.Invoke(); }, null)
         {
-
         }
 
         public event EventHandler CanExecuteChanged;
