108db98386e840ad5d5218c89a9aa161e613aedb
Nachiappan <nachiapan@gmail.com>
2019-01-26 11:05:55 +0530

    Implemented Core library for testing

---
+++ DeviceControlApp.Core/TestStatic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceControlApp.Core
+{
+    public class TestStatic
+    {
+        public static int MyInt { get; set; }
+    }
+
+    public class TestObject
+    {
+        public int MyInt { get; set; }
+    }
+}
--- DeviceControlApp/ViewModel/HomePageViewModel.cs
+++ DeviceControlApp/ViewModel/HomePageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Input;
+using DeviceControlApp.Core;
 using DeviceControlApp.Services;
 using DeviceControlApp.View;
 using Xamarin.Forms;
@@ -17,9 +18,25 @@ namespace DeviceControlApp.ViewModel
 
         public HomePageViewModel(IPageService pageService,ILocationService locationService)
         {
+            TestStatic.MyInt = 6;
+            if (TestStatic.MyInt != 6) throw new Exception("Static is not working");
+
+            TestObject testObject1 = new TestObject();
+            testObject1.MyInt = 9;
+
+            TestObject testObject2 = new TestObject();
+            testObject2.MyInt = 10;
+
+            if (testObject1.MyInt != 9) throw new Exception("Object is not working");
+
+
+
             _pageService = pageService;
             _locationService = locationService;
             GoToNextCommand = new RelayCommand(GoToNextPage);
+
+
+
         }
 
         public async void GoToNextPage()
